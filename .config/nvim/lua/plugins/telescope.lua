55271d386e92db097dbb99d611bc9d041ccbfc03
marmos91 <m.marmos@gmail.com>
2025-01-22 15:12:19 +0100

    fix nvim

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -67,7 +67,6 @@ return {
             pcall(require("telescope").load_extension, "ui-select")
             pcall(require("telescope").load_extension, "noice")
             pcall(require("telescope").load_extension, "live_grep_args")
-            pcall(require("telescope").load_extension, "chezmoi")
 
             -- See `:help telescope.builtin`
             local builtin = require("telescope.builtin")
@@ -79,12 +78,6 @@ return {
             vim.keymap.set("n", "<leader>sg", builtin.live_grep, { desc = "[S]earch by [G]rep" })
             vim.keymap.set("n", "<leader>sd", builtin.diagnostics, { desc = "[S]earch [D]iagnostics" })
             vim.keymap.set("n", "<leader>sr", builtin.resume, { desc = "[S]earch [R]esume" })
-            vim.keymap.set(
-                "n",
-                "<leader>sc",
-                require("telescope").extensions.chezmoi.find_files,
-                { desc = "[S]earch [C]hezmoi" }
-            )
             vim.keymap.set("n", "<leader>s.", builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
             vim.keymap.set("n", "<leader><leader>", builtin.buffers, { desc = "[ ] Find existing buffers" })
 
