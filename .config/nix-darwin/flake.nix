89767634e8cacd597eee4852f626a28e60b3dbcf
marmos91 <marco.moschettini@cubbit.io>
2025-08-25 13:00:52 +0200

    feat: add claude-code

--- .config/nix-darwin/flake.nix
+++ .config/nix-darwin/flake.nix
@@ -9,32 +9,36 @@
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+    claude-code.url = "github:sadjow/claude-code-nix";
   };
 
-  outputs = inputs@{ self, nix-darwin, nixpkgs, home-manager, ... }: {
-    nixpkgs.config.allowUnfree = true;
+  outputs =
+    inputs@{ self, nix-darwin, nixpkgs, claude-code, home-manager, ... }: {
+      nixpkgs.config.allowUnfree = true;
 
-    darwinConfigurations = {
-      amaterasu = nix-darwin.lib.darwinSystem {
-        pkgs = import nixpkgs {
-          system = "aarch64-darwin";
-          config.allowUnfree = true;
+      darwinConfigurations = {
+        amaterasu = nix-darwin.lib.darwinSystem {
+          pkgs = import nixpkgs {
+            system = "aarch64-darwin";
+            config.allowUnfree = true;
+          };
+          modules = [
+            ./darwin.nix
+            home-manager.darwinModules.home-manager
+            {
+              nixpkgs.overlays = [ claude-code.overlays.default ];
+
+              home-manager.useGlobalPkgs = true;
+              home-manager.useUserPackages = true;
+              home-manager.backupFileExtension = "backup";
+
+              home-manager.users.marmos91 = import ./home.nix;
+
+              # Optionally, use home-manager.extraSpecialArgs to pass
+              # arguments to home.nix
+            }
+          ];
         };
-        modules = [
-          ./darwin.nix
-          home-manager.darwinModules.home-manager
-          {
-            home-manager.useGlobalPkgs = true;
-            home-manager.useUserPackages = true;
-            home-manager.backupFileExtension = "backup";
-
-            home-manager.users.marmos91 = import ./home.nix;
-
-            # Optionally, use home-manager.extraSpecialArgs to pass
-            # arguments to home.nix
-          }
-        ];
       };
     };
-  };
 }
