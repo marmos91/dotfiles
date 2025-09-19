c2d55fc6382bb87c68bba5baf7857e76a6e4d620
Marco Moschettini <2795616+marmos91@users.noreply.github.com>
2025-09-19 17:10:03 +0200

    fix: git ssh warning

--- .config/nix-darwin/home.nix
+++ .config/nix-darwin/home.nix
@@ -311,10 +311,25 @@
 
   programs.ssh = {
     enable = true;
-    extraConfig = ''
-      Host *
-        IdentityAgent "~/Library/Group Containers/2BUA8C4S2C.com.1password/t/agent.sock"
-    '';
+    enableDefaultConfig = false;
+
+    matchBlocks = {
+      "*" = {
+        identityAgent =
+          "~/Library/Group Containers/2BUA8C4S2C.com.1password/t/agent.sock";
+
+        forwardAgent = false;
+        serverAliveInterval = 0;
+        serverAliveCountMax = 3;
+        compression = false;
+        addKeysToAgent = "no";
+        hashKnownHosts = false;
+        userKnownHostsFile = "~/.ssh/known_hosts";
+        controlMaster = "no";
+        controlPath = "~/.ssh/master-%r@%n:%p";
+        controlPersist = "no";
+      };
+    };
   };
 
   programs.git = {
