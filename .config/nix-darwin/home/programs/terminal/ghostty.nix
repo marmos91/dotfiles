81cea663ac2af4eb79168884c20de58541d12cd3
Marco Moschettini <m.marmos@gmail.com>
2025-09-24 17:22:58 +0200

    fix: fix ghostty

--- .config/nix-darwin/home/programs/terminal/ghostty.nix
+++ .config/nix-darwin/home/programs/terminal/ghostty.nix
@@ -1,19 +1,21 @@
-{ ... }: {
+{ ... }:
+{
   programs.ghostty = {
     enable = true;
     package = null; # Use the Cask version
-    enableZshIntegration = true;
 
-    settings = {
-      theme = "Catppuccin Mocha";
-      font-family = "MesloLGS Nerd Font Mono";
-      font-size = 13;
-      macos-option-as-alt = true;
-      background-opacity = 0.96;
-      background-blur-radius = 40;
-      confirm-close-surface = false;
-      mouse-hide-while-typing = true;
-      macos-titlebar-style = "tabs";
-    };
+    enableZshIntegration = true;
   };
+
+  home.file."Library/Application Support/com.mitchellh.ghostty/config".text = ''
+    theme = Catppuccin Mocha
+    font-family = MesloLGS Nerd Font Mono
+    font-size = 13
+    macos-option-as-alt = true
+    background-opacity = 0.96
+    background-blur-radius = 40
+    confirm-close-surface = false
+    mouse-hide-while-typing = true
+    macos-titlebar-style = tabs
+  '';
 }
