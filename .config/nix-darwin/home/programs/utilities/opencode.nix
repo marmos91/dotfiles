ea23d9bdeb249f475b5cecffbdd7a35da2a4d157
Marco Moschettini <2795616+marmos91@users.noreply.github.com>
2026-03-12 10:16:52 +0100

    fix: opencode configuration

--- .config/nix-darwin/home/programs/utilities/opencode.nix
+++ .config/nix-darwin/home/programs/utilities/opencode.nix
@@ -11,14 +11,14 @@
           apiKey = "{file:${config.sops.secrets.mimir_api_key.path}}";
         };
         models = {
-          glm = {
-            name = "GLM";
+          qwen = {
+            name = "Qwen3.5-122B";
             reasoning = true;
             tool_call = true;
           };
         };
       };
     };
-    model = "cubbit/glm";
+    model = "cubbit/qwen";
   };
 }
